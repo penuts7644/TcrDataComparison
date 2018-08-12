004f0593c0872d64f1baa61d677206756dbd8024
Wout van Helvoirt <wout.van.helvoirt@icloud.com>
2018-08-12 17:02:59 +0200

    Stop codon finder now works for nt_sequence and nt_CDR3 columns.

--- IgorOutputStopCodonFinder.py
+++ IgorOutputStopCodonFinder.py
@@ -18,15 +18,23 @@ def main():
     # Locate all stop codons in the given file and return the file index,
     # codon start and end index as well as the codon itself for each of them.
     print("Given sequences file path: {}".format(sys.argv[1]))
+
+    # TODO: Some CSV files are not consistent with the separator ';' or ','.
+
     df = pandas.read_csv(sys.argv[1], sep=";", index_col="seq_index", header=0)
     output_filename = sys.argv[1].split("/")[-1].split(".")[0] \
         + "_stop_codon_indices.csv"
     data_list = list()
     for i, row in df.iterrows():
         stop_codon_pattern = re.compile("[UT]AA|[UT]AG|[UT]GA")
+        search_column = ""
+        if "nt_sequence" in df.columns:
+            search_column = "nt_sequence"
+        elif "nt_CDR3" in df.columns:
+            search_column = "nt_CDR3"
         matches = [(m.start(0), m.end(0), m.group(0))
                    for m in re.finditer(stop_codon_pattern,
-                                        row["nt_sequence"])]
+                                        row[search_column])]
         data_list.append([i, matches])
     new_df = pandas.DataFrame.from_records(data_list,
                                            columns=[df.index.name,
