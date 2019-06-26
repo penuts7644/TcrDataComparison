e1624333b6a28afab7c4d841d5399fbe6e319b14
Wout van Helvoirt <wout.van.helvoirt@icloud.com>
2019-06-26 10:39:28 +0200

    file location fix

--- bash/calc_model_entropy_20files_sbatch.sh
+++ bash/calc_model_entropy_20files_sbatch.sh
@@ -24,16 +24,16 @@ cd ${SCRATCH}
 cp -r ${SLURM_SUBMIT_DIR}/TcrDataComparison/python/model_processing .
 
 # In a loop copy over necessary model files and create command string
-UNPRODUCTIVE_MODELS_VARIABLE=""
-PRODUCTIVE_MODELS_VARIABLE=""
-ALL_MODELS_VARIABLE=""
+UNPRODUCTIVE_MODELS=""
+PRODUCTIVE_MODELS=""
+ALL_MODELS=""
 for i in {0..19}
 do
     mkdir "model_${i}"
     cp "${SLURM_SUBMIT_DIR}/immuno_probs_20files_models/immuno_probs_${i}/model/." "model_${i}/."
-    UNPRODUCTIVE_MODELS_VARIABLE+=" -model ${i}_unproductive model_${i}/unproductive_params.txt model_${i}/unproductive_marginals.txt"
-    PRODUCTIVE_MODELS_VARIABLE+=" -model ${i}_productive model_${i}/productive_params.txt model_${i}/productive_marginals.txt"
-    ALL_MODELS_VARIABLE+=" -model ${i}_all model_${i}/all_params.txt model_${i}/all_marginals.txt"
+    UNPRODUCTIVE_MODELS+=" -model ${i}_unproductive ../model_${i}/unproductive_params.txt ../model_${i}/unproductive_marginals.txt"
+    PRODUCTIVE_MODELS+=" -model ${i}_productive ../model_${i}/productive_params.txt ../model_${i}/productive_marginals.txt"
+    ALL_MODELS+=" -model ${i}_all ../model_${i}/all_params.txt ../model_${i}/all_marginals.txt"
 done
 
 # Mark the output dir for automatic copying to $SLURM_SUBMIT_DIR afterwards
@@ -42,11 +42,11 @@ savefile model_entropy
 # Calculate entropy for all the models
 mkdir model_entropy
 cd model_entropy
-python ../model_processing/CalcModelEntropy.py --num-threads ${OMP_NUM_THREADS} ${UNPRODUCTIVE_MODELS_VARIABLE} &> 'unproductive_model_entropy_log.txt'
+python ../model_processing/CalcModelEntropy.py --num-threads ${OMP_NUM_THREADS} ${UNPRODUCTIVE_MODELS} &> 'unproductive_model_entropy_log.txt'
 mv calc_model_entropy.tsv unproductive_calc_model_entropy.tsv
-python ../model_processing/CalcModelEntropy.py --num-threads ${OMP_NUM_THREADS} ${PRODUCTIVE_MODELS_VARIABLE} &> 'productive_model_entropy_log.txt'
+python ../model_processing/CalcModelEntropy.py --num-threads ${OMP_NUM_THREADS} ${PRODUCTIVE_MODELS} &> 'productive_model_entropy_log.txt'
 mv calc_model_entropy.tsv productive_calc_model_entropy.tsv
-python ../model_processing/CalcModelEntropy.py --num-threads ${OMP_NUM_THREADS} ${ALL_MODELS_VARIABLE} &> 'all_model_entropy_log.txt'
+python ../model_processing/CalcModelEntropy.py --num-threads ${OMP_NUM_THREADS} ${ALL_MODELS} &> 'all_model_entropy_log.txt'
 mv calc_model_entropy.tsv all_calc_model_entropy.tsv
 cd ../
 
