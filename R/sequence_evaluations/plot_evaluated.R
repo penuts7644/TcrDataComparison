d92aa0b0873b35bbc0a1be04ec58f8c797565967
Wout van Helvoirt <wout.van.helvoirt@icloud.com>
2019-08-26 09:25:23 +0200

    Combined all plot variable script into their respective R plot script

--- R/sequence_evaluations/plot_evaluated.R
+++ R/sequence_evaluations/plot_evaluated.R
@@ -1,8 +1,60 @@
+# ----------
+# GENERAL
+# ----------
 library(GGally)
+library(reshape2)
+SUBSET_IDS <- c('all', '100000', '50000', '10000', '5000', '1000', '500')
+TYPES <- c('productive', 'unproductive', 'all')
 
 # ----------
-# READ IN DATA FOR GIVEN PRODUCTIVE, UNPRODUCTIVE OR ALL MODELS, 'plot_evaluated_variables.R'
+# FUNCTIONS FOR PRE-PROCESSING
 # ----------
+normalize <- function(x) {
+  return ((x - min(x)) / (max(x) - min(x)))
+}
+
+process_model <- function(type, subset) {
+  cc <- c(NA, rep("NULL", 6), rep(NA, 2))
+  if (type == 'productive') {
+    model_0 <- melt(data.frame(read.table(paste('~/Downloads/process_5_files/', subset, '/evaluated_CDR3/evaluate_0/pgen_estimate_productive_CDR3.tsv', sep = ''), header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc)))
+    model_1 <- melt(data.frame(read.table(paste('~/Downloads/process_5_files/', subset, '/evaluated_CDR3/evaluate_1/pgen_estimate_productive_CDR3.tsv', sep = ''), header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc)))
+    model_2 <- melt(data.frame(read.table(paste('~/Downloads/process_5_files/', subset, '/evaluated_CDR3/evaluate_2/pgen_estimate_productive_CDR3.tsv', sep = ''), header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc)))
+    model_3 <- melt(data.frame(read.table(paste('~/Downloads/process_5_files/', subset, '/evaluated_CDR3/evaluate_3/pgen_estimate_productive_CDR3.tsv', sep = ''), header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc)))
+    model_4 <- melt(data.frame(read.table(paste('~/Downloads/process_5_files/', subset, '/evaluated_CDR3/evaluate_4/pgen_estimate_productive_CDR3.tsv', sep = ''), header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc)))
+    model_combined <- melt(data.frame(read.table(paste('~/Downloads/process_5_files/', subset, '/evaluated_CDR3/evaluate_combined/pgen_estimate_productive_CDR3.tsv', sep = ''), header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc)))
+    model_default <- melt(data.frame(read.table(paste('~/Downloads/process_5_files/', subset, '/evaluated_CDR3/evaluate_igor/pgen_estimate_CDR3.tsv', sep = ''), header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc)))
+  } else if (type == 'unproductive') {
+    model_0 <- melt(data.frame(read.table(paste('~/Downloads/process_5_files/', subset, '/evaluated_CDR3/evaluate_0/pgen_estimate_unproductive_CDR3.tsv', sep = ''), header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc)))
+    model_1 <- melt(data.frame(read.table(paste('~/Downloads/process_5_files/', subset, '/evaluated_CDR3/evaluate_1/pgen_estimate_unproductive_CDR3.tsv', sep = ''), header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc)))
+    model_2 <- melt(data.frame(read.table(paste('~/Downloads/process_5_files/', subset, '/evaluated_CDR3/evaluate_2/pgen_estimate_unproductive_CDR3.tsv', sep = ''), header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc)))
+    model_3 <- melt(data.frame(read.table(paste('~/Downloads/process_5_files/', subset, '/evaluated_CDR3/evaluate_3/pgen_estimate_unproductive_CDR3.tsv', sep = ''), header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc)))
+    model_4 <- melt(data.frame(read.table(paste('~/Downloads/process_5_files/', subset, '/evaluated_CDR3/evaluate_4/pgen_estimate_unproductive_CDR3.tsv', sep = ''), header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc)))
+    model_combined <- melt(data.frame(read.table(paste('~/Downloads/process_5_files/', subset, '/evaluated_CDR3/evaluate_combined/pgen_estimate_unproductive_CDR3.tsv', sep = ''), header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc)))
+    model_default <- melt(data.frame(read.table(paste('~/Downloads/process_5_files/', subset, '/evaluated_CDR3/evaluate_igor/pgen_estimate_CDR3.tsv', sep = ''), header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc)))
+  } else if (type == 'all') {
+    model_0 <- melt(data.frame(read.table(paste('~/Downloads/process_5_files/', subset, '/evaluated_CDR3/evaluate_0/pgen_estimate_all_CDR3.tsv', sep = ''), header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc)))
+    model_1 <- melt(data.frame(read.table(paste('~/Downloads/process_5_files/', subset, '/evaluated_CDR3/evaluate_1/pgen_estimate_all_CDR3.tsv', sep = ''), header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc)))
+    model_2 <- melt(data.frame(read.table(paste('~/Downloads/process_5_files/', subset, '/evaluated_CDR3/evaluate_2/pgen_estimate_all_CDR3.tsv', sep = ''), header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc)))
+    model_3 <- melt(data.frame(read.table(paste('~/Downloads/process_5_files/', subset, '/evaluated_CDR3/evaluate_3/pgen_estimate_all_CDR3.tsv', sep = ''), header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc)))
+    model_4 <- melt(data.frame(read.table(paste('~/Downloads/process_5_files/', subset, '/evaluated_CDR3/evaluate_4/pgen_estimate_all_CDR3.tsv', sep = ''), header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc)))
+    model_combined <- melt(data.frame(read.table(paste('~/Downloads/process_5_files/', subset, '/evaluated_CDR3/evaluate_combined/pgen_estimate_all_CDR3.tsv', sep = ''), header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc)))
+    model_default <- melt(data.frame(read.table(paste('~/Downloads/process_5_files/', subset, '/evaluated_CDR3/evaluate_igor/pgen_estimate_CDR3.tsv', sep = ''), header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc)))
+  }
+  models <- na.omit(do.call('cbind', list(
+    model_0, model_1[2], model_2[2], model_3[2],
+    model_4[2], model_combined[2], model_default[2]
+  )))
+  rm(cc, model_0, model_1, model_2, model_3, model_4, model_combined, model_default)
+  names(models) <- c('type', '0', '1', '2', '3', '4', 'combined', 'default')
+  models$type <- as.character(models$type)
+  models$type[models$type == 'nt_pgen_estimate'] <- 'NT'
+  models$type[models$type == 'aa_pgen_estimate'] <- 'AA'
+  models$type <- as.factor(models$type)
+  models[models$type == 'NT', -1] <- apply(models[models$type == 'NT', -1], 2, normalize)
+  models[models$type == 'AA', -1] <- apply(models[models$type == 'AA', -1], 2, normalize)
+  models <- models[sample(nrow(models)), ]
+  return (models)
+}
 
 upper_plot_fn <- function(data, mapping, ...){
   ggally_cor(
@@ -13,18 +65,18 @@ upper_plot_fn <- function(data, mapping, ...){
     alignPercent = 1,
     size = 6
   ) +
-  theme(
-    panel.grid.major = element_blank(),
-    axis.ticks = element_blank(),
-    panel.border = element_rect(
-      linetype = "dashed",
-      colour = "gray",
-      fill = NA
+    theme(
+      panel.grid.major = element_blank(),
+      axis.ticks = element_blank(),
+      panel.border = element_rect(
+        linetype = "dashed",
+        colour = "gray",
+        fill = NA
+      )
+    ) +
+    scale_color_manual(
+      values = c('#1f78b4', '#d95f02')
     )
-  ) +
-  scale_color_manual(
-    values = c('#1f78b4', '#d95f02')
-  )
 }
 
 lower_plot_fn <- function(data, mapping, ...){
@@ -32,76 +84,91 @@ lower_plot_fn <- function(data, mapping, ...){
     data = data,
     mapping = mapping
   ) +
-  geom_point(
-    size = 2,
-    alpha = 0.4
-  ) +
-  geom_smooth(
-    size = 1,
-    method = lm,
-    se = FALSE,
-    fullrange = TRUE
-  ) +
-  xlim(0, 1) +
-  ylim(0, 1) +
-  scale_color_manual(
-    values = c('#1f78b4', '#d95f02')
-  )
+    geom_point(
+      size = 2,
+      alpha = 0.4
+    ) +
+    geom_smooth(
+      size = 1,
+      method = lm,
+      se = FALSE,
+      fullrange = TRUE
+    ) +
+    xlim(0, 1) +
+    ylim(0, 1) +
+    scale_color_manual(
+      values = c('#1f78b4', '#d95f02')
+    )
 }
 
-legend_plot <- ggplot(data = models) +
-  geom_point(
-    aes(x = `0`, y = `1`, color = type)
-  ) +
-  theme(
-    legend.text = element_text(
-      size = 15
-    ),
-    legend.position = 'bottom',
-    legend.title = element_blank(),
-    legend.direction = 'horizontal'
-  ) +
-  scale_color_manual(
-    values = c('#1f78b4', '#d95f02')
-  )
+for (subset in SUBSET_IDS) {
+  for (type in TYPES) {
 
-eval_compare <-
-  ggpairs(
-    models,
-    columns = 2:ncol(models),
-    mapping = aes(color = type),
-    axisLabels = 'show',
-    upper = list(continuous = upper_plot_fn),
-    diag = list(continuous = 'blankDiag'),
-    lower = list(continuous = lower_plot_fn),
-    progress = TRUE,
-    legend = grab_legend(legend_plot)
-  ) +
-  theme_bw(
-    plot.title = element_text(
-      size = 20
-    ),
-    axis.text.x = element_text(
-      size = 11,
-      angle = 45,
-      hjust = 1
-    ),
-    axis.text.y = element_text(
-      size = 11
-    ),
-    strip.text.x = element_text(
-      size = 15
-    ),
-    strip.text.y = element_text(
-      size = 15
-    ),
-    legend.position = 'bottom',
-    strip.placement = 'outside'
-  ) +
-  labs(
-    title = plot_title
-  )
+    # ----------
+    # EXTRACTING THE DATA
+    # ----------
+    models <- process_model(type, subset)
+    plot_title <- 'Normalized AA and NT sequence pgen compared between models'
+    output_filename <- paste('~/Downloads/process_5_files/', subset, '/evaluated_CDR3/evaluated_seqs_', type, '_rplot_', subset, '.png', sep = '')
 
-jpeg(output_filename, width = 4000, height = 4000, res = 300)
-eval_compare
-dev.off()
+    # ----------
+    # MAKING THE PLOTS
+    # ----------
+    legend_plot <- ggplot(data = models) +
+      geom_point(
+        aes(x = `0`, y = `1`, color = type)
+      ) +
+      theme(
+        legend.text = element_text(
+          size = 15
+        ),
+        legend.position = 'bottom',
+        legend.title = element_blank(),
+        legend.direction = 'horizontal'
+      ) +
+      scale_color_manual(
+        values = c('#1f78b4', '#d95f02')
+      )
+
+    eval_compare <-
+      ggpairs(
+        models,
+        columns = 2:ncol(models),
+        mapping = aes(color = type),
+        axisLabels = 'show',
+        upper = list(continuous = upper_plot_fn),
+        diag = list(continuous = 'blankDiag'),
+        lower = list(continuous = lower_plot_fn),
+        progress = TRUE,
+        legend = grab_legend(legend_plot)
+      ) +
+      theme_bw(
+        plot.title = element_text(
+          size = 20
+        ),
+        axis.text.x = element_text(
+          size = 11,
+          angle = 45,
+          hjust = 1
+        ),
+        axis.text.y = element_text(
+          size = 11
+        ),
+        strip.text.x = element_text(
+          size = 15
+        ),
+        strip.text.y = element_text(
+          size = 15
+        ),
+        legend.position = 'bottom',
+        strip.placement = 'outside'
+      ) +
+      labs(
+        title = plot_title
+      )
+
+    jpeg(output_filename, width = 4000, height = 4000, res = 300)
+    eval_compare
+    dev.off()
+  }
+}
--- R/sequence_file_sizes/plot_seq_count.R
+++ R/sequence_file_sizes/plot_seq_count.R
@@ -1,9 +1,60 @@
+# ----------
+# GENERAL
+# ----------
 library(ggplot2)
 
 # ----------
-# READ IN DATA FOR THE SEQUENCE COUNTS, 'plot_seq_count_variables.R'
+# PLOT VARIABLES
 # ----------
+plot_title <- 'Training sequence count for subject-specific models'
+output_filename <- '~/Downloads/process_5_files/sequence_counts_rplot.png'
 
+# ----------
+# FUNCTIONS FOR PRE-PROCESSING
+# ----------
+normalize <- function(x) {
+  return ((0.75 - 0.15) * ((x - min(x)) / (max(x) - min(x))) + 0.15)
+}
+
+# ----------
+# DATA PROCESSING
+# ----------
+counts <- data.frame(read.table('~/Downloads/process_5_files/sequence_counts.tsv', header=TRUE, sep='\t', check.names=FALSE))
+counts <- counts[counts$type != 'evaluated', ]
+counts <- counts[counts$model != 'combined', ]
+counts$id_f = factor(counts$id, levels=c('500','1000','5000','10000', '50000', '100000', 'all'))
+
+counts[counts$id == 'all' & counts$type == 'productive', 'norm'] <- normalize(counts[counts$id == 'all' & counts$type == 'productive', 'count'])
+counts[counts$id == 'all' & counts$type == 'unproductive', 'norm'] <- normalize(counts[counts$id == 'all' & counts$type == 'unproductive', 'count'])
+counts[counts$id == 'all' & counts$type == 'all', 'norm'] <- normalize(counts[counts$id == 'all' & counts$type == 'all', 'count'])
+
+counts[counts$id == '100000' & counts$type == 'productive', 'norm'] <- normalize(counts[counts$id == '100000' & counts$type == 'productive', 'count'])
+counts[counts$id == '100000' & counts$type == 'unproductive', 'norm'] <- normalize(counts[counts$id == '100000' & counts$type == 'unproductive', 'count'])
+counts[counts$id == '100000' & counts$type == 'all', 'norm'] <- normalize(counts[counts$id == '100000' & counts$type == 'all', 'count'])
+
+counts[counts$id == '50000' & counts$type == 'productive', 'norm'] <- normalize(counts[counts$id == '50000' & counts$type == 'productive', 'count'])
+counts[counts$id == '50000' & counts$type == 'unproductive', 'norm'] <- normalize(counts[counts$id == '50000' & counts$type == 'unproductive', 'count'])
+counts[counts$id == '50000' & counts$type == 'all', 'norm'] <- normalize(counts[counts$id == '50000' & counts$type == 'all', 'count'])
+
+counts[counts$id == '10000' & counts$type == 'productive', 'norm'] <- normalize(counts[counts$id == '10000' & counts$type == 'productive', 'count'])
+counts[counts$id == '10000' & counts$type == 'unproductive', 'norm'] <- normalize(counts[counts$id == '10000' & counts$type == 'unproductive', 'count'])
+counts[counts$id == '10000' & counts$type == 'all', 'norm'] <- normalize(counts[counts$id == '10000' & counts$type == 'all', 'count'])
+
+counts[counts$id == '5000' & counts$type == 'productive', 'norm'] <- normalize(counts[counts$id == '5000' & counts$type == 'productive', 'count'])
+counts[counts$id == '5000' & counts$type == 'unproductive', 'norm'] <- normalize(counts[counts$id == '5000' & counts$type == 'unproductive', 'count'])
+counts[counts$id == '5000' & counts$type == 'all', 'norm'] <- normalize(counts[counts$id == '5000' & counts$type == 'all', 'count'])
+
+counts[counts$id == '1000' & counts$type == 'productive', 'norm'] <- normalize(counts[counts$id == '1000' & counts$type == 'productive', 'count'])
+counts[counts$id == '1000' & counts$type == 'unproductive', 'norm'] <- normalize(counts[counts$id == '1000' & counts$type == 'unproductive', 'count'])
+counts[counts$id == '1000' & counts$type == 'all', 'norm'] <- normalize(counts[counts$id == '1000' & counts$type == 'all', 'count'])
+
+counts[counts$id == '500' & counts$type == 'productive', 'norm'] <- normalize(counts[counts$id == '500' & counts$type == 'productive', 'count'])
+counts[counts$id == '500' & counts$type == 'unproductive', 'norm'] <- normalize(counts[counts$id == '500' & counts$type == 'unproductive', 'count'])
+counts[counts$id == '500' & counts$type == 'all', 'norm'] <- normalize(counts[counts$id == '500' & counts$type == 'all', 'count'])
+
+# ----------
+# MAKING THE PLOT
+# ----------
 size_compare <-
   ggplot(
     counts,
