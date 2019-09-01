11a21f779b3501b4773a107a54e5333396fc8eeb
Wout van Helvoirt <wout.van.helvoirt@icloud.com>
2019-09-01 18:54:55 +0200

    small fixes to claim 1 R scripts

--- R/claim_1/model_entropies_plot.R
+++ R/claim_1/model_entropies_plot.R
@@ -8,7 +8,7 @@ library(ggplot2)
 # FUNCTIONS FOR PRE-PROCESSING
 # ----------
 process_entropies <- function(data) {
-  subject_list <- c('0', 'subject 0', '1', 'subject 1', '2', 'subject 2', '3', 'subject 3', '4', 'subject 4')
+  subject_list <- c('0', 'disease 1', '1', 'disease 2', '2', 'disease 3', '3', 'control 1', '4', 'control 2')
   frame_list <- c('productive', 'in', 'unproductive', 'out/stop', 'all', 'in/out/stop')
   tmp <- subset(data, grepl('0', Var1) & grepl('0', Var2) & Var1 != Var2)
   for (i in c('1', '2', '3', '4')) {
@@ -22,7 +22,7 @@ process_entropies <- function(data) {
     tmp$L4[grepl(frame_list[i], tmp$Var2)] <- frame_list[i+1]
   }
   names(tmp)[4] <- "Event level"
-  names(tmp)[5] <- "Subject ID"
+  names(tmp)[5] <- "Model ID"
   names(tmp)[6] <- "Frame ID"
   names(tmp)[7] <- "Frame type"
   tmp <- tmp[sample(nrow(tmp)), ]
@@ -32,8 +32,8 @@ process_entropies <- function(data) {
 # ----------
 # PLOT VARIABLES
 # ----------
-plot_title <- 'Subject event-level entropy comparison between sequence frames'
-plot_y <- 'combined KL divergence (bits)'
+plot_y <- 'Combined KL divergence (bits)'
+plot_x <- 'Event levels'
 output_filename <- '~/Downloads/claim_1/model_entropies_plot.png'
 
 # ----------
@@ -70,9 +70,6 @@ rm(V, D, J, V_trim_3, D_trim_3, D_trim_5, J_trim_5, insert_length_VD, insert_len
 # ----------
 # MAKING THE PLOTS
 # ----------
-# ----------
-# MAKING THE PLOTS
-# ----------
 entr_compare <-
   ggplot(
     entropies,
@@ -84,6 +81,7 @@ entr_compare <-
   geom_boxplot(
     alpha = 0.5,
     width = 0.6,
+    outlier.size = 0.2,
     outlier.shape = NA
   ) +
   geom_jitter(
@@ -91,16 +89,17 @@ entr_compare <-
     width = .2,
     size = 4,
     aes(
-      color = `Frame type`
+      color = `Frame type`,
+      shape = `Frame type`
     ),
     alpha = 1
   ) +
   theme_bw() +
   theme(
-    plot.title = element_text(
-      size = 20
+    plot.title = element_blank(),
+    axis.title.x = element_text(
+      size = 18
     ),
-    axis.title.x = element_blank(),
     axis.text.x = element_text(
       size = 13,
       angle = 60,
@@ -124,18 +123,18 @@ entr_compare <-
     legend.text = element_text(
       size = 15
     ),
-    legend.position = "bottom",
+    legend.position = "top",
     legend.direction = "horizontal"
   ) +
   labs(
-    title = plot_title,
-    y = plot_y
+    y = plot_y,
+    x = plot_x
   ) +
-  scale_color_manual(
-    values = c('#1f78b4', '#d95f02', '#33a02c')
+  scale_color_brewer(
+    palette = 'Dark2'
   ) +
   facet_grid(
-    rows = vars(`Subject ID`),
+    rows = vars(`Model ID`),
     cols = vars(`Frame ID`)
   )
 
--- R/claim_1/sequence_evaluation_plot.R
+++ R/claim_1/sequence_evaluation_plot.R
@@ -25,6 +25,7 @@ process_model <- function(x, y, combination, name) {
   model$corr.AA[model$type == 'AA'] <- c(cor(model[model$type == 'AA', 2], model[model$type == 'AA', 3], method = 'spearman'), rep(NA, nrow(model[model$type == 'AA', ]) - 1))
   model$combination <- combination
   model$name <- name
+  names(model)[1] <- "Sequence type"
   model <- model[sample(nrow(model)), ]
   return (model)
 }
@@ -32,7 +33,8 @@ process_model <- function(x, y, combination, name) {
 # ----------
 # PLOT VARIABLES
 # ----------
-plot_title <- 'Normalized AA and NT sequence pgen compared for each model version'
+plot_y <- 'Pgen score (normalized)'
+plot_x <- 'Pgen score (normalized)'
 output_filename <- '~/Downloads/claim_1/sequence_evaluation_plot.png'
 
 # ----------
@@ -43,45 +45,45 @@ cc <- c(NA, rep("NULL", 6), rep(NA, 2))
 model_0_productive <- data.frame(read.table('~/Downloads/claim_1/evaluations/subject_0/pgen_estimate_productive_CDR3.tsv', header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc))
 model_0_unproductive <- data.frame(read.table('~/Downloads/claim_1/evaluations/subject_0/pgen_estimate_unproductive_CDR3.tsv', header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc))
 model_0_all <- data.frame(read.table('~/Downloads/claim_1/evaluations/subject_0/pgen_estimate_all_CDR3.tsv', header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc))
-model_0_p_a <- process_model(model_0_productive, model_0_all, 'productive - all', '0')
-model_0_p_u <- process_model(model_0_productive, model_0_unproductive, 'productive - unproductive', '0')
-model_0_a_u <- process_model(model_0_all, model_0_unproductive, 'all - unproductive', '0')
+model_0_p_a <- process_model(model_0_productive, model_0_all, 'in (X) - in/out/stop (Y)', 'disease 1')
+model_0_p_u <- process_model(model_0_productive, model_0_unproductive, 'in (X) - out/stop (Y)', 'disease 1')
+model_0_a_u <- process_model(model_0_all, model_0_unproductive, 'in/out/stop (X) - out/stop (Y)', 'disease 1')
 model_0 <- as.data.frame(do.call("rbind", list(model_0_p_a, model_0_p_u, model_0_a_u)))
 rm(model_0_productive, model_0_unproductive, model_0_all, model_0_p_a, model_0_p_u, model_0_a_u)
 
 model_1_productive <- data.frame(read.table('~/Downloads/claim_1/evaluations/subject_1/pgen_estimate_productive_CDR3.tsv', header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc))
 model_1_unproductive <- data.frame(read.table('~/Downloads/claim_1/evaluations/subject_1/pgen_estimate_unproductive_CDR3.tsv', header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc))
 model_1_all <- data.frame(read.table('~/Downloads/claim_1/evaluations/subject_1/pgen_estimate_all_CDR3.tsv', header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc))
-model_1_p_a <- process_model(model_1_productive, model_1_all, 'productive - all', '1')
-model_1_p_u <- process_model(model_1_productive, model_1_unproductive, 'productive - unproductive', '1')
-model_1_a_u <- process_model(model_1_all, model_1_unproductive, 'all - unproductive', '1')
+model_1_p_a <- process_model(model_1_productive, model_1_all, 'in (X) - in/out/stop (Y)', 'disease 2')
+model_1_p_u <- process_model(model_1_productive, model_1_unproductive, 'in (X) - out/stop (Y)', 'disease 2')
+model_1_a_u <- process_model(model_1_all, model_1_unproductive, 'in/out/stop (X) - out/stop (Y)', 'disease 2')
 model_1 <- as.data.frame(do.call("rbind", list(model_1_p_a, model_1_p_u, model_1_a_u)))
 rm(model_1_productive, model_1_unproductive, model_1_all, model_1_p_a, model_1_p_u, model_1_a_u)
 
 model_2_productive <- data.frame(read.table('~/Downloads/claim_1/evaluations/subject_2/pgen_estimate_productive_CDR3.tsv', header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc))
 model_2_unproductive <- data.frame(read.table('~/Downloads/claim_1/evaluations/subject_2/pgen_estimate_unproductive_CDR3.tsv', header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc))
 model_2_all <- data.frame(read.table('~/Downloads/claim_1/evaluations/subject_2/pgen_estimate_all_CDR3.tsv', header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc))
-model_2_p_a <- process_model(model_2_productive, model_2_all, 'productive - all', '2')
-model_2_p_u <- process_model(model_2_productive, model_2_unproductive, 'productive - unproductive', '2')
-model_2_a_u <- process_model(model_2_all, model_2_unproductive, 'all - unproductive', '2')
+model_2_p_a <- process_model(model_2_productive, model_2_all, 'in (X) - in/out/stop (Y)', 'disease 3')
+model_2_p_u <- process_model(model_2_productive, model_2_unproductive, 'in (X) - out/stop (Y)', 'disease 3')
+model_2_a_u <- process_model(model_2_all, model_2_unproductive, 'in/out/stop (X) - out/stop (Y)', 'disease 3')
 model_2 <- as.data.frame(do.call("rbind", list(model_2_p_a, model_2_p_u, model_2_a_u)))
 rm(model_2_productive, model_2_unproductive, model_2_all, model_2_p_a, model_2_p_u, model_2_a_u)
 
 model_3_productive <- data.frame(read.table('~/Downloads/claim_1/evaluations/subject_3/pgen_estimate_productive_CDR3.tsv', header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc))
 model_3_unproductive <- data.frame(read.table('~/Downloads/claim_1/evaluations/subject_3/pgen_estimate_unproductive_CDR3.tsv', header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc))
 model_3_all <- data.frame(read.table('~/Downloads/claim_1/evaluations/subject_3/pgen_estimate_all_CDR3.tsv', header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc))
-model_3_p_a <- process_model(model_3_productive, model_3_all, 'productive - all', '3')
-model_3_p_u <- process_model(model_3_productive, model_3_unproductive, 'productive - unproductive', '3')
-model_3_a_u <- process_model(model_3_all, model_3_unproductive, 'all - unproductive', '3')
+model_3_p_a <- process_model(model_3_productive, model_3_all, 'in (X) - in/out/stop (Y)', 'control 1')
+model_3_p_u <- process_model(model_3_productive, model_3_unproductive, 'in (X) - out/stop (Y)', 'control 1')
+model_3_a_u <- process_model(model_3_all, model_3_unproductive, 'in/out/stop (X) - out/stop (Y)', 'control 1')
 model_3 <- as.data.frame(do.call("rbind", list(model_3_p_a, model_3_p_u, model_3_a_u)))
 rm(model_3_productive, model_3_unproductive, model_3_all, model_3_p_a, model_3_p_u, model_3_a_u)
 
 model_4_productive <- data.frame(read.table('~/Downloads/claim_1/evaluations/subject_4/pgen_estimate_productive_CDR3.tsv', header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc))
 model_4_unproductive <- data.frame(read.table('~/Downloads/claim_1/evaluations/subject_4/pgen_estimate_unproductive_CDR3.tsv', header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc))
-model_4_all <- data.frame(read.table('~/Downloads/pclaim_1/evaluations/subject_4/pgen_estimate_all_CDR3.tsv', header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc))
-model_4_p_a <- process_model(model_4_productive, model_4_all, 'productive - all', '4')
-model_4_p_u <- process_model(model_4_productive, model_4_unproductive, 'productive - unproductive', '4')
-model_4_a_u <- process_model(model_4_all, model_4_unproductive, 'all - unproductive', '4')
+model_4_all <- data.frame(read.table('~/Downloads/claim_1/evaluations/subject_4/pgen_estimate_all_CDR3.tsv', header=TRUE, row.names=1, sep='\t', check.names=FALSE, colClasses=cc))
+model_4_p_a <- process_model(model_4_productive, model_4_all, 'in (X) - in/out/stop (Y)', 'control 2')
+model_4_p_u <- process_model(model_4_productive, model_4_unproductive, 'in (X) - out/stop (Y)', 'control 2')
+model_4_a_u <- process_model(model_4_all, model_4_unproductive, 'in/out/stop (X) - out/stop (Y)', 'control 2')
 model_4 <- as.data.frame(do.call("rbind", list(model_4_p_a, model_4_p_u, model_4_a_u)))
 rm(model_4_productive, model_4_unproductive, model_4_all, model_4_p_a, model_4_p_u, model_4_a_u)
 
@@ -97,7 +99,8 @@ eval_compare <-
     aes(
       x = X,
       y = Y,
-      color = type
+      color = `Sequence type`,
+      shape = `Sequence type`
     )
   ) +
   geom_point(
@@ -137,22 +140,28 @@ eval_compare <-
     fontface = "bold",
     show.legend = FALSE
   ) +
-  theme_bw(
-    plot.title = element_text(
-      size = 20
+  theme_bw() +
+  theme(
+    plot.title = element_blank(),
+    legend.title = element_text(
+      size = 18
     ),
     legend.text = element_text(
       size = 15
     ),
-    axis.title.x = element_blank(),
-    axis.title.y = element_blank(),
+    axis.title.x = element_text(
+      size = 18
+    ),
     axis.text.x = element_text(
-      size = 11,
-      angle = 45,
+      size = 13,
+      angle = 60,
       hjust = 1
     ),
+    axis.title.y = element_text(
+      size = 18
+    ),
     axis.text.y = element_text(
-      size = 11
+      size = 13
     ),
     strip.text.x = element_text(
       size = 15
@@ -160,17 +169,17 @@ eval_compare <-
     strip.text.y = element_text(
       size = 15
     ),
-    legend.position = 'bottom',
-    legend.title = element_blank(),
+    legend.position = 'top',
     legend.direction = 'horizontal'
   ) +
   xlim(0, 1) +
   ylim(0, 1) +
   labs(
-    title = plot_title
+    y = plot_y,
+    x = plot_x
   ) +
-  scale_color_manual(
-    values = c('#1f78b4', '#d95f02')
+  scale_color_brewer(
+    palette = 'Dark2'
   ) +
   facet_grid(
     rows = vars(name),
